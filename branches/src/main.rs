386f61c034963205b2ac0ad1b192b6b6fe5c9b34
VicvekSr <user@USERs-MacBook-Pro.local>
2024-02-20 21:34:06 +0100

    add control flow

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -7,4 +7,24 @@ fn main() {
     } else {
         println!("condition was false");
     }
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    let b = [10, 20, 30, 40, 50];
+
+    for element in b {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..10).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
