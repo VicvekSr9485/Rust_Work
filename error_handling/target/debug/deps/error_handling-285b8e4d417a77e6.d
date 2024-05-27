df5562e50d0332005226ad72ecbac17acb42e061
VicvekSr <user@VicvekSr-MacBook-Pro.local>
2024-05-27 14:54:07 +0100

    error handling with panic and Result<T, E>

---
+++ error_handling/target/debug/deps/error_handling-285b8e4d417a77e6.d
@@ -0,0 +1,5 @@
+/Users/user/rust_work/error_handling/target/debug/deps/liberror_handling-285b8e4d417a77e6.rmeta: src/main.rs
+
+/Users/user/rust_work/error_handling/target/debug/deps/error_handling-285b8e4d417a77e6.d: src/main.rs
+
+src/main.rs:
---
+++ error_handling/target/debug/deps/error_handling-a2492c41b24a0252.d
@@ -0,0 +1,5 @@
+/Users/user/rust_work/error_handling/target/debug/deps/liberror_handling-a2492c41b24a0252.rmeta: src/main.rs
+
+/Users/user/rust_work/error_handling/target/debug/deps/error_handling-a2492c41b24a0252.d: src/main.rs
+
+src/main.rs:
