0ebdcac5648e25c2ef313523928c58e4193f052e
VicvekSr <user@VicvekSr-MacBook-Pro.local>
2024-06-02 05:37:16 +0100

    writing automated tests

--- automated_tests/adder/src/lib.rs
+++ automated_tests/adder/src/lib.rs
@@ -33,6 +33,12 @@ impl Rectangle {
     }
 }
 
+// test to show function output with `cargo test -- --show-output`
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -91,5 +97,17 @@ mod tests {
         } else {
             Err(String::from("two plus two does not equal four"))
         }
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
