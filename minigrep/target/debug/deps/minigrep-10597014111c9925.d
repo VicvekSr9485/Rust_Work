15b9096a9f4ff4ba02c5b5773c8238dd3479b648
VicvekSr <user@VicvekSr-MacBook-Pro.local>
2024-06-05 15:36:38 +0100

    complete minigrep

---
+++ minigrep/target/debug/deps/minigrep-10597014111c9925.d
@@ -0,0 +1,5 @@
+/Users/user/rust_work/minigrep/target/debug/deps/minigrep-10597014111c9925: src/lib.rs
+
+/Users/user/rust_work/minigrep/target/debug/deps/minigrep-10597014111c9925.d: src/lib.rs
+
+src/lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-8adfcbd79b5af86e.d
@@ -0,0 +1,5 @@
+/Users/user/rust_work/minigrep/target/debug/deps/minigrep-8adfcbd79b5af86e: src/main.rs
+
+/Users/user/rust_work/minigrep/target/debug/deps/minigrep-8adfcbd79b5af86e.d: src/main.rs
+
+src/main.rs:
