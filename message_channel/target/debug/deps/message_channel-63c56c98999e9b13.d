de94e090988e2b00ee86cf1f60f13d102ae5b948
VicvekSr <user@VicvekSr-MacBook-Pro.local>
2024-06-15 20:47:00 +0100

    add fearless concurrency methods

---
+++ message_channel/target/debug/deps/message_channel-63c56c98999e9b13.d
@@ -0,0 +1,5 @@
+/Users/user/rust_work/message_channel/target/debug/deps/message_channel-63c56c98999e9b13: src/main.rs
+
+/Users/user/rust_work/message_channel/target/debug/deps/message_channel-63c56c98999e9b13.d: src/main.rs
+
+src/main.rs:
---
+++ message_channel/target/debug/message_channel.d
@@ -0,0 +1,1 @@
+/Users/user/rust_work/message_channel/target/debug/message_channel: /Users/user/rust_work/message_channel/src/main.rs
